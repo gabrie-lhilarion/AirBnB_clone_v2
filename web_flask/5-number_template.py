e7827526657633c1410e910a9f027cb588362e7e
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-04-21 20:05:34 +0100

    Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+This script creates a simple Flask web
+application that responds with "Hello HBNB!" when accessed.
+"""
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Route handler function for the root URL.
+
+    Returns:
+        str: A simple greeting message.
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route handler function for the /hbnb URL.
+
+    Returns:
+        str: HBNB.
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/', strict_slashes=False)
+def c(text='is_cool'):
+    """
+    Route handler function for the /c/.
+
+    Returns:
+        str: c is cool or c <provided test>.
+    """
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python(text='is_cool'):
+    """
+    Route handler function for the /pyton/.
+
+    Returns:
+        str: Python is cool or Python <provided test>.
+    """
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Route handler function for the /number/n
+
+    Returns:
+        str: <n> is a number or 404.
+    """
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
+    else:
+        return 'Not an integer', 400
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
